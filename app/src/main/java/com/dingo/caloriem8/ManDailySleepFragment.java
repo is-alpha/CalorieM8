1d81b3b5e570dbf7b46b7e5ac3580b28b7e4d3af
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2020-05-22 20:02:39 -0700

    Sleep home fragment

---
+++ app/src/main/java/com/dingo/caloriem8/ManDailySleepFragment.java
@@ -0,0 +1,25 @@
+package com.dingo.caloriem8;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ManDailySleepFragment extends Fragment {
+
+    public ManDailySleepFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_man_daily_sleep, container, false);
+    }
+}
