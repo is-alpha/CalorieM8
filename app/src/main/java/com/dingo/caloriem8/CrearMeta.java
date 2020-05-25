ff7fab09e160ad6ec9ea8f8fe05cceac6fc7f38c
jungeumii <kim.jungeum@uabc.edu.mx>
2020-05-25 16:48:49 -0700

    METAS
    Ya puedes meter datos, pero todavia no valido el boton de SUMMIT

--- app/src/main/java/com/dingo/caloriem8/CrearMeta.java
+++ app/src/main/java/com/dingo/caloriem8/CrearMeta.java
@@ -1,64 +1,126 @@
 package com.dingo.caloriem8;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CrearMeta#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CrearMeta extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseAuth fAuth;
+    private DatabaseReference dbRef;
+    private int d;
+    private EditText start_date;
+    private EditText end_date;
+    private DatePickerDialog.OnDateSetListener dp_dateSetListener;
+    private DatePickerDialog.OnDateSetListener dp_dateSetListener2;
+    private Context currContext;
+    private String date;
 
     public CrearMeta() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CrearMeta.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CrearMeta newInstance(String param1, String param2) {
-        CrearMeta fragment = new CrearMeta();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear_meta, container, false);
+        View view = inflater.inflate(R.layout.fragment_crear_meta, container, false);
+        fAuth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
+        start_date = view.findViewById(R.id.et_start_date);
+
+        start_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                d = 1;
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(currContext,
+                        R.style.Theme_AppCompat_Light_Dialog,
+                        dp_dateSetListener,
+                        year, month, day);
+                dialog.getWindow();
+                dialog.show();
+            }
+        });
+
+        end_date = view.findViewById(R.id.et_end_date);
+        end_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                d = 2;
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(currContext,
+                        R.style.Theme_AppCompat_Light_Dialog,
+                        dp_dateSetListener,
+                        year, month, day);
+                dialog.getWindow();
+                dialog.show();
+            }
+        });
+
+
+        dp_dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                date = month + "/" + dayOfMonth + "/" + year;
+                switch (d){
+                    case 1: start_date.setText(date);break;
+                    case 2: end_date.setText(date); break;
+
+                }
+
+            }
+        };
+
+
+        return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        currContext = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        currContext = null;
+    }
+
+
 }
--- app/src/main/java/com/dingo/caloriem8/MetasFragment.java
+++ app/src/main/java/com/dingo/caloriem8/MetasFragment.java
@@ -7,58 +7,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MetasFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class MetasFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DatabaseReference dbRef;
+    private FirebaseAuth fAuth;
+    private Button btnIMC;
 
     public MetasFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MetasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MetasFragment newInstance(String param1, String param2) {
-        MetasFragment fragment = new MetasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_metas, container, false);
+        View view = inflater.inflate(R.layout.fragment_metas, container, false);
+        fAuth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        btnIMC = view.findViewById(R.id.frp_btnNewMeta);
+
+        btnIMC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.main_fragment_container, new CrearMeta()).commit();
+            }
+        });
+
+        return  view;
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+    }
+
 }
