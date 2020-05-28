2d59a1128d51089cb4c2ed95e77d4fe1838a767d
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2020-05-27 19:20:02 -0700

    Revert "Revert "Daily Weight Capture""
    This reverts commit 356e561e9f9badf0528b17c2dfb646e22167d625.

--- app/src/main/java/com/dingo/caloriem8/HomeFragment.java
+++ app/src/main/java/com/dingo/caloriem8/HomeFragment.java
@@ -84,6 +84,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
                     break;
 
                 case 2:
+                    ManDailyWeightFragment manDailyWeightFrag= new ManDailyWeightFragment();
+                    ft.replace(R.id.home_fragment_container, manDailyWeightFrag);
                     break;
                 case 3:
                     break;
