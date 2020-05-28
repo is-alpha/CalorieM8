60562c709373606a3e43fdcb42e4f130bf714d16
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2020-05-27 20:21:19 -0700

    fix 2

--- app/src/main/java/com/dingo/caloriem8/ManDailyWeightFragment.java
+++ app/src/main/java/com/dingo/caloriem8/ManDailyWeightFragment.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
@@ -29,24 +30,21 @@ import java.util.Calendar;
 import java.util.Date;
 
 
-public class ManDailySleepFragment extends Fragment implements View.OnClickListener {
+public class ManDailyWeightFragment extends Fragment implements View.OnClickListener {
     private Context currContext;
 
-    private FirebaseAuth fAuth;
     private DatabaseReference dbRef;
-    private Date dateToday;
-    private String todayStdDateFormat;
+    private FirebaseAuth fAuth;
 
     private DayInfo dayInfo;
     private String dayKey;
+    private Date dateToday;
+    private String todayStdDateFormat;
 
-    private TextView date;
-    private Button btnAvgPlus;
-    private Button btnAvgMinus;
-    private Button btnExtraPlus;
-    private Button btnExtraMinus;
-    private EditText etAvg;
-    private EditText etExtra;
+    private TextView tvDate;
+    private EditText etWeight;
+    private Button btnDec;
+    private Button btnInc;
     private Button btnSubmit;
 
     @Override
@@ -59,23 +57,6 @@ public class ManDailySleepFragment extends Fragment implements View.OnClickListe
         dateToday = Calendar.getInstance().getTime();
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         todayStdDateFormat = df.format(dateToday);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_man_daily_sleep, container, false);
-
-        date = view.findViewById(R.id.dayinfo_date_sleep);
-        btnAvgMinus = view.findViewById(R.id.dayinfo_add_minus_sleep);
-        btnAvgPlus = view.findViewById(R.id.dayinfo_add_plus_sleep);
-        btnExtraMinus = view.findViewById(R.id.dayinfo_sub_minus_sleep);
-        btnExtraPlus = view.findViewById(R.id.dayinfo_sub_plus_sleep);
-        etAvg = view.findViewById(R.id.dayinfo_edit_add_sleep);
-        etExtra = view.findViewById(R.id.dayinfo_edit_sub_sleep);
-        btnSubmit = view.findViewById(R.id.dayinfo_btnSubmit_sleep);
-
-        SimpleDateFormat df = new SimpleDateFormat("E MMM dd yyyy");
-        date.setText(df.format(dateToday));
 
         dbRef.child("DayInfo").child(fAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
             @Override
@@ -91,16 +72,14 @@ public class ManDailySleepFragment extends Fragment implements View.OnClickListe
                 dayKey = next.getKey();
 
                 if(dayInfo.getDate().equals(todayStdDateFormat)) {
-                    etAvg.setText(dayInfo.getAvgSleep());
-                    etExtra.setText(dayInfo.getExtraSleep());
+                    etWeight.setText(dayInfo.getWeight());
                 } else {
                     dayKey = Integer.toString(Integer.parseInt(dayKey) + 1);
                     DayInfo todayValue = new DayInfo();
                     todayValue.setDate(todayStdDateFormat);
-                    if(prev != null){
+                    if(prev != null) {
                         DayInfo prevDayInfo = prev.getValue(DayInfo.class);
-                        todayValue.setAvgSleep(prevDayInfo.getAvgSleep());
-                        todayValue.setExtraSleep(prevDayInfo.getExtraSleep());
+                        todayValue.setWeight(prevDayInfo.getWeight());
                     }
                     dbRef.child("DayInfo").child(fAuth.getCurrentUser().getUid()).child(dayKey).setValue(todayValue);
                 }
@@ -111,39 +90,40 @@ public class ManDailySleepFragment extends Fragment implements View.OnClickListe
 
             }
         });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_man_daily_weight, container, false);
+
+        tvDate = view.findViewById(R.id.dayinfo_date_weight);
+        etWeight = view.findViewById(R.id.dayinfo_edit_weight);
+        btnDec = view.findViewById(R.id.dayinfo_dec_weight);
+        btnInc = view.findViewById(R.id.dayinfo_inc_weight);
+        btnSubmit = view.findViewById(R.id.dayinfo_btnSubmit_weight);
+
+        SimpleDateFormat df = new SimpleDateFormat("E MMM dd yyyy");
+        tvDate.setText(df.format(dateToday));
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String average = etAvg.getText().toString().trim();
-                String extra = etExtra.getText().toString().trim();
+                String weight = etWeight.getText().toString().trim();
 
-                if(TextUtils.isEmpty(average)) {
-                    etAvg.setError("Invalid");
+                if(TextUtils.isEmpty(weight)) {
+                    etWeight.setError("Invalid");
                     return;
                 }
                 try {
-                    Integer.parseInt(average);
+                    Integer.parseInt(weight);
                 } catch (NumberFormatException e) {
-                    etAvg.setError("Invalid");
-                    return;
-                }
-
-                if(TextUtils.isEmpty(extra)) {
-                    etExtra.setError("Invalid");
-                    return;
-                }
-                try {
-                    Integer.parseInt(extra);
-                } catch (NumberFormatException e) {
-                    etExtra.setError("Invalid");
+                    etWeight.setError("Invalid");
                     return;
                 }
 
                 DayInfo updatedInfo = dayInfo;
                 updatedInfo.setDate(todayStdDateFormat);
-                updatedInfo.setAvgSleep(average);
-                updatedInfo.setExtraSleep(extra);
+                updatedInfo.setWeight(weight);
                 dbRef.child("DayInfo").child(fAuth.getCurrentUser().getUid()).child(dayKey).setValue(updatedInfo).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
@@ -153,35 +133,26 @@ public class ManDailySleepFragment extends Fragment implements View.OnClickListe
             }
         });
 
-        btnAvgPlus.setOnClickListener(this);
-        btnAvgMinus.setOnClickListener(this);
-        btnExtraPlus.setOnClickListener(this);
-        btnExtraMinus.setOnClickListener(this);
-
+        btnInc.setOnClickListener(this);
+        btnDec.setOnClickListener(this);
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        int step = 1, average, extra;
+        int step = 1, weight;
         try {
-            average = Integer.parseInt(etAvg.getText().toString().trim());
-            extra = Integer.parseInt(etExtra.getText().toString().trim());
+            weight = Integer.parseInt(etWeight.getText().toString().trim());
         } catch (NumberFormatException ex) {
             ex.printStackTrace();
             return;
         }
 
-        if(v == btnAvgPlus) {
-            etAvg.setText(Integer.toString(average + step));
-        } else if(v == btnAvgMinus) {
-            if(average - step > 0)
-                etAvg.setText(Integer.toString(average - step));
-        } else if(v == btnExtraPlus) {
-            etExtra.setText(Integer.toString(extra + step));
-        } else if(v == btnExtraMinus) {
-            if(extra - step > 0)
-                etExtra.setText(Integer.toString(extra - step));
+        if(v == btnInc) {
+            etWeight.setText(Integer.toString(weight + step));
+        } else if(v == btnDec) {
+            if(weight - step > 0)
+                etWeight.setText(Integer.toString(weight - step));
         }
     }
 
