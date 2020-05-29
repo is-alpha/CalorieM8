1754fd160edf41047b065296a698466fcebcb4f0
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2020-05-28 21:40:43 -0700

    DietFragment
    + Added Diet Screen

---
+++ app/src/main/java/com/dingo/caloriem8/AddNewFoodFragment.java
@@ -0,0 +1,23 @@
+package com.dingo.caloriem8;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddNewFoodFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_new_food, container, false);
+
+        return view;
+    }
+}
