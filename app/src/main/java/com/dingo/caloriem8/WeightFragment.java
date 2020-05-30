df26e6b41b8b52cbd2b6e530460c743e30b0d9a2
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2020-05-30 00:09:15 -0700

    Update WeightFragment.java
    + Now retreives weight values from Firebase DB.

--- app/src/main/java/com/dingo/caloriem8/WeightFragment.java
+++ app/src/main/java/com/dingo/caloriem8/WeightFragment.java
@@ -28,7 +28,13 @@ import java.util.Date;
 
 
 public class WeightFragment extends Fragment {
+
+    interface WeightFragCallback {
+        void onCompleteAction(DayInfo ant, DayInfo act);
+    }
+    private WeightFragCallback callback;
     private Context currContext;
+    private View view;
     private FirebaseAuth fAuth;
     private DatabaseReference dbRef;
     String fecha;
@@ -47,36 +53,14 @@ public class WeightFragment extends Fragment {
         dbRef = FirebaseDatabase.getInstance().getReference();
 
         weight_ant = new DayInfo();
-        weight_ant.setWeight("0");
         weight_act = new DayInfo();
-        weight_act.setWeight("0");
-
-        dbRef.child("DayInfo").child(fAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                DayInfo tmp;
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                    tmp = ds.getValue(DayInfo.class);
-                    if(!tmp.getWeight().equals("null")) {
-                        weight_ant = new DayInfo(weight_act);
-                        weight_act = new DayInfo(tmp);
-                    }
-                    Toast.makeText(currContext, "WEIGHT = " + weight_act.getWeight(), Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_weight, container, false);
+        view = inflater.inflate(R.layout.fragment_weight, container, false);
 
         /* Boton para regresar a la pestaña anterior*/
         iv_goBack = view.findViewById(R.id.iv_goBack);
@@ -87,41 +71,82 @@ public class WeightFragment extends Fragment {
             }
         });
 
+        callback = new WeightFragCallback() {
+            @Override
+            public void onCompleteAction(DayInfo ant, DayInfo act) {
+                weight_ant = new DayInfo(ant);
+                weight_act = new DayInfo(act);
+
+                if(weight_ant.getWeight().equals("null"))
+                    weight_ant.setWeight("0");
+                if(weight_act.getWeight().equals("null"))
+                    weight_act.setWeight("0");
+
+                tv_weightAct = view.findViewById(R.id.weight_act);
+                tv_weightAct.setText(weight_act.getWeight() + " Kg");
+
+                tv_weightAnt = view.findViewById(R.id.weight_ant);
+                tv_weightAnt.setText(weight_ant.getWeight() + " Kg");
+
+                //operacion de difericencia
+
+                res = Integer.parseInt(weight_act.getWeight()) - Integer.parseInt(weight_ant.getWeight());
+
+                tv_message = view.findViewById(R.id.message_weight);
+                tv_difWeight = view.findViewById(R.id.frw_Difweight);
+
+                if(res < 0)
+                {
+                    tv_message.setText("You've gained weight !");
+                    tv_difWeight.setText("-" + Integer.toString(res) + " Kg");
+                }
+                else if(res > 0){
+                    tv_message.setText("You've lost weight !");
+                    tv_difWeight.setText("+" + Integer.toString(res) + " Kg");
+                    tv_difWeight.setTextColor(Color.RED);
+                }
+                else if(res == 0)
+                {
+                    tv_message.setText("You weight is the same !");
+                    tv_difWeight.setText(Integer.toString(res) + " Kg");
+                }
+            }
+        };
+
+        dbRef.child("DayInfo").child(fAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            /*No se puede asignar valor dentro de esto porque los Listeners de Firebase son async
+             * se debe usar una funcion all back, variables y sus valores asignados estan limitados al scope de este listener
+             * NO BORRAR COMENTARIO, PORQUE ES EN MEMORIA A T0DO EL TIEMPO QUE LE DEDIQUE A ESTA BURRADA
+             * AL MENOS APRENDI ESTE DETALLE IMPORTANTE
+             * REF: https://stackoverflow.com/questions/39209823/setting-variable-inside-ondatachange-in-firebase-singlevalue-listener*/
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                DayInfo tmp, ant, act;
+                ant = new DayInfo();
+                act = new DayInfo();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    tmp = ds.getValue(DayInfo.class);
+                    if(!tmp.getWeight().equals("null")) {
+                        ant = new DayInfo(act);
+                        act = new DayInfo(tmp);
+                    }
+//                    Toast.makeText(currContext, "WEIGHT = " + weight_act.getWeight(), Toast.LENGTH_SHORT).show();
+                }
+                callback.onCompleteAction(ant, act);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         SimpleDateFormat sdf = new SimpleDateFormat("E MMMM dd yyyy");
         date = Calendar.getInstance().getTime();
         fecha = sdf.format(date);
 
         tv_fecha = view.findViewById(R.id.id_fecha);
         tv_fecha.setText(fecha);
-
-        tv_weightAct = view.findViewById(R.id.weight_act);
-        tv_weightAct.setText(weight_act.getWeight() + " Kg");
-
-        tv_weightAnt = view.findViewById(R.id.weight_ant);
-        tv_weightAnt.setText(weight_ant.getWeight() + " Kg");
-
-        //operacion de difericencia
-
-        res = Integer.parseInt(weight_act.getWeight()) - Integer.parseInt(weight_ant.getWeight());
-
-        tv_message = view.findViewById(R.id.message_weight);
-        tv_difWeight = view.findViewById(R.id.frw_Difweight);
-
-        if(res < 0)
-        {
-            tv_message.setText("You've gained weight !");
-            tv_difWeight.setText("-" + Integer.toString(res) + " Kg");
-        }
-        else if(res > 0){
-            tv_message.setText("You've lost weight !");
-            tv_difWeight.setText("+" + Integer.toString(res) + " Kg");
-            tv_difWeight.setTextColor(Color.RED);
-        }
-        else if(res == 0)
-        {
-            tv_message.setText("You weight is the same !");
-            tv_difWeight.setText(Integer.toString(res) + " Kg");
-        }
 
         return view;
     }
