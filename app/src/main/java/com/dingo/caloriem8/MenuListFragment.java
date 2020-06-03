01414514c2e726400e52408b6bccd87ef2aee0a7
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2020-06-02 21:33:33 -0700

    Send menu list through Email

--- app/src/main/java/com/dingo/caloriem8/MenuListFragment.java
+++ app/src/main/java/com/dingo/caloriem8/MenuListFragment.java
@@ -1,9 +1,12 @@
 package com.dingo.caloriem8;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,18 +15,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
 /*https://github.com/codepath/android-custom-array-adapter-demo/tree/master/app/src/main/java/com/codepath/example/customadapterdemo*/
 
 public class MenuListFragment extends Fragment {
+    private FirebaseAuth fAuth;
     private CalM8SQLiteHelper calM8SQLiteHelper;
     private Context currContext;
     private ListView listView;
     private TextView tvCalCount;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         fAuth = FirebaseAuth.getInstance();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
@@ -45,6 +59,14 @@ public class MenuListFragment extends Fragment {
         listView.setAdapter(getUpdatedAdapter(view));
 
         updateCalorieCount(view);
+
+        Button btnSendEmail = view.findViewById(R.id.fml_send_email);
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
         //ListView lvMenuList;
         //RecyclerView rvMenuList;
 
@@ -72,6 +94,37 @@ public class MenuListFragment extends Fragment {
             calCount += food.getCalories();
         }
         tvCalCount.setText("Calorie Count: " + Integer.toString(calCount));
+    }
+
+    private void sendEmail() {
+        String recipient = fAuth.getCurrentUser().getEmail();
+        String subject = "CalorieM8 - Today's Menu List";
+        StringBuilder msgBuilder = new StringBuilder();
+
+        for(Food food : calM8SQLiteHelper.getAllFoods()) {
+            msgBuilder.append(food.getName() + "\r\n");
+            msgBuilder.append("\tServing: "  + food.getServing() + "g\r\n");
+            msgBuilder.append("\tCalories: " + food.getCalories() + "\r\n");
+            msgBuilder.append("\tFat: " + food.getFat() + "g\r\n");
+            msgBuilder.append("\tCarbohydrates: " + food.getCarbs() + "g\r\n");
+            msgBuilder.append("\tFiber: " + food.getFiber() + "g\r\n");
+            msgBuilder.append("\tProtein: " + food.getProtein() + "g\r\n");
+            msgBuilder.append("\r\n");
+        }
+
+        String message = msgBuilder.toString();
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, recipient);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+        emailIntent.setType("message/rfc822");
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Choose an Email client"));
+        }catch(android.content.ActivityNotFoundException ex) {
+            Toast.makeText(currContext, "No Email clients installed", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
